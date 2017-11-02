4f231cd2d788c9cbff3bcfcdb1b215de6a82f7d5
Mike Hawes <mikehawes@ww203174.wwnet.wne.edu>
2017-11-02 14:51:58 -0400

    Adding file

---
+++ main/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render_to_response
+from django.contrib.staticfiles.views import serve
+
+def staticFile(request,fname):
+    result = serve(request,fname)
+    return result
+
+def index(request):
+    return render_to_response('index.html')
